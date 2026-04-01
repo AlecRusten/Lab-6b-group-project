bcc03534f7124970627909780cfabb9a6b9ac34e
RileyVangurd <rileyvangurd@gmail.com>
2026-03-31 21:01:22 -0500

    Update 1
    Added character class, began work on it

---
+++ GroupProject/src/Character.java
@@ -0,0 +1,12 @@
+public class Character {
+    String Name;
+    int Health;
+    int RegenSpeed;
+
+    public Character(String name, int health, int regenspeed)
+        {
+            Name = name;
+            Health = health;
+            RegenSpeed = regenspeed;
+        }
+}
