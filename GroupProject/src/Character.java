0d219f069cf59eee22441b0e74f006bcc557ac87
RileyVangurd <rileyvangurd@gmail.com>
2026-04-03 19:29:06 -0500

    Update 2
    Character.java updated

--- GroupProject/src/Character.java
+++ GroupProject/src/Character.java
@@ -1,8 +1,9 @@
-public class character{
+public class Character{
     private String Name;
     private int Health;
     private int Strength;
     private int Speed;
+    private int CritChance = 10;
 
     private static final int IN_HP = 100;
     private static final int IN_ATK = 5;
@@ -12,6 +13,23 @@ public class character{
         {
             Name = name;
             Health = health;
-            RegenSpeed = regenspeed;
+            Speed = regenspeed;
         }
+
+        public String getName() {
+            return Name;
+        }
+
+        public int getHealth() {
+            return Health;
+        }
+
+        public int getSpeed() {
+            return Speed;
+        }
+
+        public int getCrit() {
+            return CritChance;
+        }
+        
 }
